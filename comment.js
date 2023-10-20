140f6095016036ef0fd396096cadc8d98691d256
QuendoDev <58349008+QuendoDev@users.noreply.github.com>
2023-10-20 02:45:40 +0000

    Copilot thirdd commit

--- comment.js
+++ comment.js
@@ -1,72 +1,64 @@
 //Create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var multer = require('multer');
+//Load modules
+var http = require('http');
+var url = require('url');
 var fs = require('fs');
-var mysql = require('mysql');
-var pool = mysql.createPool({
-	connectionLimit: 5,
-	host: 'localhost',
-	user: 'root',
-	database: 'o2',
-	password: '111111'
-});
+var qs = require('querystring');
+var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+var db = require('./lib/db');
+var topic = require('./lib/topic');
+var author = require('./lib/author');
+var cookie = require('cookie');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.locals.pretty = true;
-app.use('/user', express.static('uploads'));
-
-app.set('views', './views_mysql');
-app.set('view engine', 'jade');
-
-app.get('/topic/add', function(req, res) {
-	var sql = 'SELECT id, title FROM topic';
-	pool.query(sql, function(err, rows, fields) {
-		if (err) {
-			console.log(err);
-			res.status(500).send('Internal Server Error');
-		}
-		res.render('add', { topics: rows });
-	});
-});
-
-app.post('/topic/add', function(req, res) {
-	var title = req.body.title;
-	var description = req.body.description;
-	var author = req.body.author;
-
-	var sql = 'INSERT INTO topic (title, description, author) VALUES (?, ?, ?)';
-	pool.query(sql, [title, description, author], function(err, rows, fields) {
-		if (err) {
-			console.log(err);
-			res.status(500).send('Internal Server Error');
-		}
-		res.redirect('/topic/' + rows.insertId);
-	});
-});
-
-app.get(['/topic/:id/edit'], function(req, res) {
-	var sql = 'SELECT id, title FROM topic';
-	pool.query(sql, function(err, rows, fields) {
-		var id = req.params.id;
-		if (id) {
-			var sql = 'SELECT * FROM topic WHERE id=?';
-			pool.query(sql, [id], function(err, rows, fields) {
-				if (err) {
-					console.log(err);
-					res.status(500).send('Internal Server Error');
-				} else {
-					res.render('edit', { topics: rows, topics2: rows[0], topics3: rows[0] });
-				}
-			});
-		} else {
-			console.log('There is no id.');
-			res.status(500).send('Internal Server Error');
-		}
-	});
-});
-
-app.post(['/topic/:id/edit'], function(req, res) {
-	var title = req;
+var app = http.createServer(function(request,response){
+    var _url = request.url; // _url = query string
+    var queryData = url.parse(_url, true).query; //queryData = query string object
+    var pathname = url.parse(_url, true).pathname; //pathname = path name
+    var title = queryData.id; //title = query string id value
+    var cookies = {};
+    if(request.headers.cookie !== undefined){
+      cookies = cookie.parse(request.headers.cookie);
+    }
+    if(pathname === '/'){
+      if(cookies.email === undefined || cookies.password === undefined){
+        author.login(request,response);
+      } else {
+        db.query(`SELECT * FROM author WHERE email=?`,[cookies.email],function(error,author){
+          if(error){
+            throw error;
+          }
+          if(author[0].password === cookies.password){
+            topic.home(request,response);
+          } else {
+            author.login(request,response);
+          }
+        });
+      }
+    } else if(pathname === '/login_process'){
+      author.login_process(request,response);
+    } else if(pathname === '/logout_process'){
+      author.logout_process(request,response);
+    } else if(pathname === '/create'){
+      topic.create(request,response);
+    } else if(pathname === '/create_process'){
+      topic.create_process(request,response);
+    } else if(pathname === '/update'){
+      topic.update(request,response);
+    } else if(pathname === '/update_process'){
+      topic.update_process(request,response);
+    } else if(pathname === '/delete_process'){
+      topic.delete_process(request,response);
+    } else if(pathname === '/author'){
+      author.home(request,response);
+    } else if(pathname === '/author/create_process'){
+      author.create_process(request,response);
+    } else if(pathname === '/author/update'){
+      author.update(request,response);
+    } else if(pathname === '/author/update_process'){
+      author.update_process
+    }
 });
